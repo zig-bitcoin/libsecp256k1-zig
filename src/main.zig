aea8c10930b53d6808b9101761de70fd6216b2b2
pascal <pascal@merkleplant.xyz>
2024-09-03 09:12:02 +0200

    fix: Use CSPRNG instead of timestamp seeded rng

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,20 @@
 const std = @import("std");
 const secp256k1 = @import("secp256k1");
 const Sha256 = std.crypto.hash.sha2.Sha256;
+const rand = std.crypto.random;
 
 pub fn generateKeypair() !void {
     const secp = try secp256k1.Secp256k1.genNew();
     defer secp.deinit();
 
-    var rng = std.Random.DefaultPrng.init(@intCast(std.time.timestamp()));
-
     // First option:
     {
-        const seckey, const pubkey = try secp.generateKeypair(rng.random());
+        const seckey, const pubkey = try secp.generateKeypair(rand);
         try std.testing.expectEqual(pubkey, secp256k1.PublicKey.fromSecretKey(secp, seckey));
     }
     // Second option:
     {
-        const seckey = secp256k1.SecretKey.generateWithRandom(rng.random());
+        const seckey = secp256k1.SecretKey.generateWithRandom(rand);
         const pubkey = secp256k1.PublicKey.fromSecretKey(secp, seckey);
         _ = pubkey; // autofix
     }
